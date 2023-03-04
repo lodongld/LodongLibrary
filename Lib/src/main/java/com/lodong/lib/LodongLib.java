3d20ee45342dbb4353a66eb1c7287b05c6fcd905
lodongld <121932332+lodongld@users.noreply.github.com>
2023-03-04 16:21:58 +0900

    1.0.0

---
+++ Lib/src/main/java/com/lodong/lib/LodongLib.java
@@ -0,0 +1,20 @@
+package com.lodong.lib;
+
+import java.text.DecimalFormat;
+
+public class LodongLib {
+
+    public String commaOnStringChangedFromInteger(Integer integer){
+        DecimalFormat df = new DecimalFormat("###,###");
+        String money = df.format(integer);
+
+        return money;
+    }
+
+    public String commaOnStringChangedFromString(String string){
+        DecimalFormat df = new DecimalFormat("###,###");
+        String money = df.format(string);
+
+        return money;
+    }
+}
