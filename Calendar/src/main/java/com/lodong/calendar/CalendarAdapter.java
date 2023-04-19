7faece1276e495d2d7e2e8330b2ae0bc8c7c290d
lodongld <121932332+lodongld@users.noreply.github.com>
2023-04-20 00:33:59 +0900

    1.3.7

--- Calendar/src/main/java/com/lodong/calendar/CalendarAdapter.java
+++ Calendar/src/main/java/com/lodong/calendar/CalendarAdapter.java
@@ -4,79 +4,81 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.Calendar;
 
-public class CalendarAdapter extends RecyclerView.Adapter<CalendarAdapter.ViewHolder> {
-    // {0 ~ 30/31} 까지의 날짜 리스트
-    private ArrayList<Day> list;
-
-    private Context context;
-    private final LayoutInflater inflater;
-
-    Calendar cal = Calendar.getInstance();
-    ItemClickListener mClickListener;
-
-    public CalendarAdapter(Context context, int year, int month, ArrayList<Day> list) {
-        this.context = context;
-        this.list = list;
-        this.inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-
-        // 캘린더 세팅
-        this.cal.set(Calendar.YEAR, year);
-        this.cal.set(Calendar.MONTH, month);
-    }
-
-    @NonNull
-    @Override
-    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view = inflater.inflate(R.layout.item_calendar, parent, false);
-        return new ViewHolder(view);
-    }
-
-    @Override
-    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-        Day day = list.get(position);
-        if (day != null) holder.tv_item.setText(day.getDay());
-    }
-
-    @Override
-    public long getItemId(int position) {
-        return position;
-    }
-
-    @Override
-    public int getItemCount() {
-        return list.size();
-    }
-
-    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
-        TextView tv_item;
-        ImageView iv_item;
-        boolean isInMonth = true;
-
-        public ViewHolder(@NonNull View itemView) {
-            super(itemView);
-            tv_item = itemView.findViewById(R.id.tv_date);
-            iv_item = itemView.findViewById(R.id.iv_date);
-            itemView.setOnClickListener(this);
-        }
-
-        public void onClick(View view) {
-            if (mClickListener != null) mClickListener.onItemClick(view, (String) tv_item.getText(), isInMonth);
-        }
-    }
-
-    // 클릭리스너 인터페이스
-    public interface ItemClickListener {
-        void onItemClick(View view, String day, boolean isInMonth);
-    }
-
-    public void setClickListener(ItemClickListener itemClickListener) {
-        this.mClickListener = itemClickListener;
-    }
-
-}
+//public class CalendarAdapter extends RecyclerView.Adapter<CalendarAdapter.ViewHolder> {
+//    // {0 ~ 30/31} 까지의 날짜 리스트
+//    private ArrayList<Day> list;
+//
+//    private Context context;
+//    private final LayoutInflater inflater;
+//
+//    Calendar cal = Calendar.getInstance();
+//    ItemClickListener mClickListener;
+//
+//    public CalendarAdapter(Context context, int year, int month, ArrayList<Day> list) {
+//        this.context = context;
+//        this.list = list;
+//        this.inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+//
+//        // 캘린더 세팅
+//        this.cal.set(Calendar.YEAR, year);
+//        this.cal.set(Calendar.MONTH, month);
+//    }
+//
+//    @NonNull
+//    @Override
+//    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+//        View view = inflater.inflate(R.layout.item_calendar, parent, false);
+//        return new ViewHolder(view);
+//    }
+//
+//    @Override
+//    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+//        Day day = list.get(position);
+//        if (day != null) holder.tv_item.setText(day.getDay());
+//    }
+//
+//    @Override
+//    public long getItemId(int position) {
+//        return position;
+//    }
+//
+//    @Override
+//    public int getItemCount() {
+//        return list.size();
+//    }
+//
+//    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+//        TextView tv_item;
+//        ImageView iv_item;
+//        boolean isInMonth = true;
+//
+//        public ViewHolder(@NonNull View itemView) {
+//            super(itemView);
+//            tv_item = itemView.findViewById(R.id.tv_date);
+//            iv_item = itemView.findViewById(R.id.iv_date);
+//            itemView.setOnClickListener(this);
+//        }
+//
+//        public void onClick(View view) {
+//            if (mClickListener != null) mClickListener.onItemClick(view, (String) tv_item.getText(), isInMonth);
+//        }
+//    }
+//
+//    // 클릭리스너 인터페이스
+//    public interface ItemClickListener {
+//        void onItemClick(View view, String day, boolean isInMonth);
+//    }
+//
+//    public void setClickListener(ItemClickListener itemClickListener) {
+//        this.mClickListener = itemClickListener;
+//    }
+//
+//}
--- Calendar/src/main/java/com/lodong/calendar/WeekAdapter.java
+++ Calendar/src/main/java/com/lodong/calendar/WeekAdapter.java
@@ -1,63 +1,63 @@
-package com.lodong.calendar;
-
-import android.content.Context;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.TextView;
-
-import androidx.annotation.NonNull;
-import androidx.recyclerview.widget.RecyclerView;
-
-import java.util.ArrayList;
-
-public class WeekAdapter extends RecyclerView.Adapter<WeekAdapter.ViewHolder> {
-
-    private ArrayList<String> list;
-    private Context context;
-    private final LayoutInflater inflater;
-
-    /**
-     * 생성자
-     *
-     * @param context
-     * @param list
-     */
-    public WeekAdapter(Context context, ArrayList<String> list) {
-        this.context = context;
-        this.list = list;
-        this.inflater = (LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-    }
-
-    @NonNull
-    @Override
-    public WeekAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view = inflater.inflate(R.layout.item_calendar_day_of_week, parent, false);
-        return new WeekAdapter.ViewHolder(view);
-    }
-
-    @Override
-    public void onBindViewHolder(@NonNull WeekAdapter.ViewHolder holder, int position) {
-        holder.tv_date_of_week.setText("" + list.get(position));
-    }
-
-    @Override
-    public long getItemId(int position) {
-        return position;
-    }
-
-    @Override
-    public int getItemCount() {
-        return list.size();
-    }
-
-
-    public class ViewHolder extends RecyclerView.ViewHolder {
-        TextView tv_date_of_week;
-
-        public ViewHolder(@NonNull View itemView) {
-            super(itemView);
-            tv_date_of_week = itemView.findViewById(R.id.tv_day_of_week);
-        }
-    }
-}
+//package com.lodong.calendar;
+//
+//import android.content.Context;
+//import android.view.LayoutInflater;
+//import android.view.View;
+//import android.view.ViewGroup;
+//import android.widget.TextView;
+//
+//import androidx.annotation.NonNull;
+//import androidx.recyclerview.widget.RecyclerView;
+//
+//import java.util.ArrayList;
+//
+//public class WeekAdapter extends RecyclerView.Adapter<WeekAdapter.ViewHolder> {
+//
+//    private ArrayList<String> list;
+//    private Context context;
+//    private final LayoutInflater inflater;
+//
+//    /**
+//     * 생성자
+//     *
+//     * @param context
+//     * @param list
+//     */
+//    public WeekAdapter(Context context, ArrayList<String> list) {
+//        this.context = context;
+//        this.list = list;
+//        this.inflater = (LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+//    }
+//
+//    @NonNull
+//    @Override
+//    public WeekAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+//        View view = inflater.inflate(R.layout.item_calendar_day_of_week, parent, false);
+//        return new WeekAdapter.ViewHolder(view);
+//    }
+//
+//    @Override
+//    public void onBindViewHolder(@NonNull WeekAdapter.ViewHolder holder, int position) {
+//        holder.tv_date_of_week.setText("" + list.get(position));
+//    }
+//
+//    @Override
+//    public long getItemId(int position) {
+//        return position;
+//    }
+//
+//    @Override
+//    public int getItemCount() {
+//        return list.size();
+//    }
+//
+//
+//    public class ViewHolder extends RecyclerView.ViewHolder {
+//        TextView tv_date_of_week;
+//
+//        public ViewHolder(@NonNull View itemView) {
+//            super(itemView);
+//            tv_date_of_week = itemView.findViewById(R.id.tv_day_of_week);
+//        }
+//    }
+//}
