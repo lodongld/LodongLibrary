22288cdc713a5b54bb0340bd697487e8ae18b75a
lodongld <121932332+lodongld@users.noreply.github.com>
2023-04-20 00:59:04 +0900

    1.3.9

---
+++ Calendar/build/generated/source/buildConfig/debug/com/lodong/calendar/BuildConfig.java
@@ -0,0 +1,10 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.lodong.calendar;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String LIBRARY_PACKAGE_NAME = "com.lodong.calendar";
+  public static final String BUILD_TYPE = "debug";
+}
